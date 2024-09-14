07be6633895d7db9e84c080588d0f3aa7183e726
Juliane Hennerich <133507690+julianebandeira@users.noreply.github.com>
2024-09-14 14:44:44 -0300

    django e app

--- siteEPIs/siteEPIs/wsgi.py
+++ siteEPIs/siteEPIs/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gerenciamentoEPIsdjango project.
+WSGI config for siteEPIs project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gerenciamentoEPIsdjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'siteEPIs.settings')
 
 application = get_wsgi_application()
